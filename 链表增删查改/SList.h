5d6ad0023a3c64b4186b9d6ce5d180e8a854bbd0
ZzW163010 <146351364+ZzW163010@users.noreply.github.com>
2023-10-31 01:24:39 +0800

    Update SList.h

--- 链表增删查改/SList.h
+++ 链表增删查改/SList.h
@@ -14,3 +14,9 @@ typedef struct SListNode
 
 void SListPushBack(SListNode** plt, SLTDataType x);
 void SListPrint(SListNode* plt);
+void SListPushFront(SListNode** plt, SLTDataType x);
+void SListPopBack(SListNode** plt);
+void SListPopFront(SListNode** plt);
+void SListInsertFront(SListNode** plt, SLTDataType x, SLTDataType y);
+void SListEraseFront(SListNode** plt, SLTDataType x);
+
