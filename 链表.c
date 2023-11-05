565e0b83d11a2a6ee1710cde432b6b8935a380b2
ZzW163010 <146351364+ZzW163010@users.noreply.github.com>
2023-11-05 13:40:44 +0800

    Update 链表.c

--- 链表.c
+++ 链表.c
@@ -47,3 +47,133 @@ struct ListNode* middleNode(struct ListNode* head){
     }
     return slow;
 }
+
+//合并两个有序链表，合并后仍然有序
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    struct ListNode* newlist = NULL;
+    struct ListNode* curn = NULL;
+    
+
+    while (list1 && list2) {
+        if (list1->val > list2->val) {
+			if (newlist == NULL) {
+				newlist = list2;
+				curn = list2;
+				list2 = list2->next;
+			}
+			else {
+				curn->next = list2;
+				list2 = list2->next;
+				curn = curn->next;
+			}
+			
+        }
+        else {
+			if (newlist == NULL) {
+				newlist = list1;
+				curn = list1;
+				list1 = list1->next;
+			}
+			else {
+				curn->next = list1;
+				list1 = list1->next;
+				curn = curn->next;
+			}
+        }
+    }
+	if (list1 != NULL) {
+		curn->next = list1;
+	}
+	if (list2 != NULL) {
+		curn->next = list2;
+	}
+   
+    return newlist;
+}
+
+//先取比较取出一个节点
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    
+    struct ListNode* newlist = NULL;
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    if (list1->val > list2->val) {
+        newlist = list2;
+        list2 = list2->next;
+    }
+    else {
+        newlist = list1;
+        list1 = list1->next;
+    }
+    struct ListNode* curn = newlist;
+
+    while (list1 && list2) {
+        if (list1->val > list2->val) {
+            curn->next = list2;
+            list2 = list2->next;
+            curn = curn->next;
+        }
+        else {
+            curn->next = list1;
+            list1 = list1->next;
+            curn = curn->next;
+        }
+    }
+    if(list1 != NULL){
+        curn->next = list1;
+    }
+    if(list2 != NULL){
+        curn->next = list2;
+    }
+     
+    return newlist;
+}
+
+//创建一个哨兵位的头结点
+
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if (list1 == NULL) {
+        return list2;
+    }
+    if (list2 == NULL) {
+        return list1;
+    }
+    struct ListNode* curn;
+    struct ListNode* newlist;
+    newlist = curn = (struct ListNode*)malloc(sizeof(struct ListNode));
+    
+    while (list1 && list2) {
+        if (list1->val > list2->val) {
+            curn->next = list2;
+            list2 = list2->next;
+            curn = curn->next;
+        }
+        else {
+            curn->next = list1;
+            list1 = list1->next;
+            curn = curn->next;
+        }
+    }
+    if(list1 != NULL){
+        curn->next = list1;
+    }
+    if(list2 != NULL){
+        curn->next = list2;
+    }
+    struct ListNode* first = newlist->next;
+    free(newlist);
+
+    return first;
+}
